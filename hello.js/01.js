56ae27d1a6dc6327b742b37c10b50668f0069276
Aman Verma <857449av@gmail.com>
2026-09-03 23:54:19 +0530

    lectiure 3 done

--- hello.js/01.js
+++ hello.js/01.js
@@ -1,2 +1,6 @@
 console.log("Hey There I am JS")
-
+// package.json file is made by node init
+// how to attach github
+// 1. git add .
+// git commit -m "TYPE MSG HERE"
+// all file in github paste here
